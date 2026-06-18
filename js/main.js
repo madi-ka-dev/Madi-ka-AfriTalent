863dcf866fa53be847f0d00616be6169c4170fd5
madi-ka-dev <madi.ka@unchk.edu.sn>
2026-06-18 21:16:13 +1245

    commit 7 : animations compteurs  + fade-in

--- js/main.js
+++ js/main.js
@@ -1,33 +1,78 @@
 const btn = document.getElementById("theme-btn");
-btn.addEventListener("click", () => {document.body.classList.toggle ("dark-mode")});
+btn.addEventListener("click", () => { document.body.classList.toggle("dark-mode") });
 // Sauvegarde
-if(document.body.classList.contains("dark-mode")){
+if (document.body.classList.contains("dark-mode")) {
     localStorage.setItem("theme", "dark");
 } else {
     localStorage.setItem("theme", "light");
 }
-window.addEventListener("scroll", () =>{
+window.addEventListener("scroll", () => {
     const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50){
+    if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
-    }else{
+    } else {
         navbar.classList.remove("scrolled");
     }
 });
 const topBtn = document.getElementById("topBtn");
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 200){
+    if (window.scrollY > 200) {
         topBtn.style.display = "block";
-    } else{
+    } else {
         topBtn.style.display = "none";
     }
 }
 );
 topBtn.addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: "smooth"});
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+// Compteurs ANIMES
+const counters = document.querySelectorAll(".counter");
+
+const startCounter = (counter) => {
+    const target = +counter.getAttribute("data-target");
+    let count = 0;
+    const updateCounter = () => {
+        const increment = target / 100;
+        if (count < target) {
+            count += increment;
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target;
+        }
+
+    };
+    updateCounter();
+};
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains("counter")) {
+                startCounter(entry.target);
+            }
+            entry.target.classList.add("show");
+        }
+    });
+});
+document.querySelectorAll(".counter, .fade-in").forEach(el => {
+    observer.observe(el);
 });
 
+// commite 7 
+const obs = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+        } else {
+            entry.target.classList.remove("visible"); 
+        }
+    });
+}, {
+});
 
+const section = document.querySelectorAll("section");
 
-        
-    
+for (let sect of section){
+    obs.observe(sect);
+}
